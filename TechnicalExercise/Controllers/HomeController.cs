d00b965e82fb6576a3631cf9f8fc0f1c4b01265e
Dominic Yeoh <yeohd100@gmail.com>
2023-01-29 00:13:33 +1100

    updates

--- TechnicalExercise/Controllers/HomeController.cs
+++ TechnicalExercise/Controllers/HomeController.cs
@@ -15,12 +15,12 @@ namespace TechnicalExercise.Controllers
             bookRepo = new BookRepository("server=localhost;user=root;database=groupfour_technical;port=3306;");
         }
 
-        public IActionResult Index(string searchString)
+        public IActionResult Index()
         {
             List<BookModel> books = bookRepo.GetBooks();
             foreach (var book in books){Console.WriteLine($"book name: {book.BookName} | bookId: {book.BookId}");}
 
-            return View(books); // Pass the list of books to the view
+            return View(books); // pass the list of books to the view
         }
 
         public IActionResult Search(string searchString) // used for search the book library
@@ -48,13 +48,13 @@ namespace TechnicalExercise.Controllers
             return View();
         }
 
-        public IActionResult ReservationFailure(string reason) // Action result to display reservation failure with reasoning so the user understands what went wrong.
+        public IActionResult ReservationFailure(string reason) // action result to display reservation failure with reasoning - so the user understands what went wrong
         {
             ViewData["reason"] = reason;
             return View();
         }
 
-        public IActionResult ReservationSuccess(string bookingNumber, string customerName, string bookName) // Action result to display reservation success
+        public IActionResult ReservationSuccess(string bookingNumber, string customerName, string bookName) // action result to display reservation success
         {
             ViewData["bookingNumber"] = bookingNumber;
             ViewData["customerName"] = customerName;
@@ -64,28 +64,12 @@ namespace TechnicalExercise.Controllers
 
         public IActionResult Reserve(Dictionary<string, string> formValues)
         {
-            string bId = String.Empty;
+            string bId = bookRepo.GetBookId(formValues); // book id
 
-            foreach (var item in formValues)
-            {
-                // Extracting bookId from index.cshtml 
-                if (item.Value != null)
-                {
-                    if (item.Key.StartsWith("customerName_"))
-                    {
-                        bId = item.Key.Replace("customerName_", "");
-                    }
-                    else if (item.Key.StartsWith("customerEmail_"))
-                    {
-                        bId = item.Key.Replace("customerEmail_", "");
-                    }
-                }
-            }
-
-            // Initial screening to check if the Dictionary contains the specific key to resolve throwing an error
+            // initial screening to check if the Dictionary contains the specific key to resolve throwing an error
             if (!formValues.ContainsKey("customerName_" + bId) || !formValues.ContainsKey("customerEmail_" + bId)) return RedirectToAction("ReservationFailure", new { reservationStatus = "failure", reason = "Your Information was not entered" }); // returns failure if the user sends an empty string
 
-            // Assigning required values to later be used to unreserve books 
+            // assigning required values to later be used to unreserve books 
             string customerName = formValues["customerName_"+ bId];
             string customerEmail = formValues["customerEmail_"+ bId];
             string bookId = formValues["bookId_" + bId];
@@ -94,21 +78,61 @@ namespace TechnicalExercise.Controllers
             if (customerName == null || customerEmail == null) return RedirectToAction("ReservationFailure", new { reservationStatus = "failure", reason = "Your Information was not entered" }); // returns failure if the user sends an empty string
 
             List<BookModel> books = bookRepo.GetBooks();
-            var book = bookRepo.GetBookById(bookId, books); // Retrieve the book that the customer wants to reserve
+            var book = bookRepo.GetBookById(bookId, books); // retrieve the book that the customer wants to reserve
 
             Console.WriteLine("reserving...");
-            if (book.Reserved == false) // Check if the book is already reserved
+            if (book.Reserved == false) // check if the book is already reserved
             {
-                // Generate a booking number
+                // generate a booking number
                 var bookingNumber = Guid.NewGuid().ToString();
 
-                // Update the book's reservation status and booking number in the database
+                // update the book's reservation status and booking number in the database
                 book.Reserved = !book.Reserved;
                 book.BookingNumber = bookingNumber;
                 book.CustomerName = customerName;
                 book.CustomerEmail = customerEmail;
 
-                bookRepo.ReserveBook(book);
+                bookRepo.ReserveBook(book); // calls the reserve method to update the mysql database field values for the specific books
+
+                return RedirectToAction("ReservationSuccess", new { reservationStatus = "success", bookingNumber = book.BookingNumber, customerName = customerName, bookName = book.BookName });
+            }
+            else
+            {
+                return RedirectToAction("ReservationFailure", new { reservationStatus = "failure", reason = $"The book '{book.BookName}' is already reserved by someone else" });
+            }
+        }
+
+        public IActionResult UnReserve(Dictionary<string, string> formValues)
+        {
+            string bId = bookRepo.GetBookId(formValues); // book id
+
+            // initial screening to check if the Dictionary contains the specific key to resolve throwing an error
+            if (!formValues.ContainsKey("customerName_" + bId) || !formValues.ContainsKey("customerEmail_" + bId)) return RedirectToAction("ReservationFailure", new { reservationStatus = "failure", reason = "Your Information was not entered" }); // returns failure if the user sends an empty string
+
+            // assigning required values to later be used to unreserve books 
+            string customerName = formValues["customerName_" + bId];
+            string customerEmail = formValues["customerEmail_" + bId];
+            string bookId = formValues["bookId_" + bId];
+
+            // returns failure message if the user sends an empty string
+            if (customerName == null || customerEmail == null) return RedirectToAction("ReservationFailure", new { reservationStatus = "failure", reason = "Your Information was not entered" }); // returns failure if the user sends an empty string
+
+            List<BookModel> books = bookRepo.GetBooks();
+            var book = bookRepo.GetBookById(bookId, books); // retrieve the book that the customer wants to reserve
+
+            Console.WriteLine("reserving...");
+            if (book.Reserved == false) // check if the book is already reserved
+            {
+                // generate a booking number
+                var bookingNumber = Guid.NewGuid().ToString();
+
+                // update the book's reservation status and booking number in the database
+                book.Reserved = !book.Reserved;
+                book.BookingNumber = bookingNumber;
+                book.CustomerName = customerName;
+                book.CustomerEmail = customerEmail;
+
+                bookRepo.UnReserveBook(book); // calls the reserve method to update the mysql database field values for the specific books
 
                 return RedirectToAction("ReservationSuccess", new { reservationStatus = "success", bookingNumber = book.BookingNumber, customerName = customerName, bookName = book.BookName });
             }
--- TechnicalExercise/Models/BookRepository.cs
+++ TechnicalExercise/Models/BookRepository.cs
@@ -10,42 +10,64 @@ namespace TechnicalExercise.Models
 
         public BookRepository(string connectionString)
         {
-            _connectionString = connectionString;
+            _connectionString = connectionString; // initialise conection string
         }
 
-        public List<TechnicalExercise.Models.BookModel> GetBooks()
+        public List<TechnicalExercise.Models.BookModel> GetBooks() // retrieves all books to be displayed in a table
         {
-            using var connection = new MySqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString); // establishes connection
             var books = new List<TechnicalExercise.Models.BookModel>();
 
             try
             {
+                // opening the connection
                 Console.WriteLine("Connecting to MySQL...");
                 connection.Open();
 
-                using var command = new MySqlCommand("SELECT * FROM books_library", connection);
-                using var reader = command.ExecuteReader();
+                using var command = new MySqlCommand("SELECT * FROM books_library", connection); // select all books from the database to be displayed
+                using var reader = command.ExecuteReader(); // returns mysql data reader object containing values from the databse
 
                 while (reader.Read())
                 {
-                    books.Add(new TechnicalExercise.Models.BookModel
+                    books.Add(new TechnicalExercise.Models.BookModel // adding each book from the MySQL database to the list of books
                     {
                         BookId = reader.GetString("id"),
                         BookName = reader.GetString("name"),
                         Reserved = reader.GetBoolean("reserved_booking"),
                     });
                 }
-                return books;
+                return books; // returns the list of books
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                
                 connection.Close(); // closing the connection
-                Console.WriteLine("Done.");
-               
-                return books;
+
+                return books; // returns empty list of books
             }
+        }
+
+        public string GetBookId(Dictionary<string, string> formValues)
+        {
+            string bookId = String.Empty;
+
+            foreach (var item in formValues)
+            {
+                // extracting bookId from index.cshtml 
+                if (item.Value != null)
+                {
+                    if (item.Key.StartsWith("customerName_"))
+                    {
+                        bookId = item.Key.Replace("customerName_", "");
+                    }
+                    else if (item.Key.StartsWith("customerEmail_"))
+                    {
+                        bookId = item.Key.Replace("customerEmail_", "");
+                    }
+                }
+            }
+
+            return bookId;
         }
 
         public BookModel GetBookById(string bookId, List<BookModel> books)
@@ -61,6 +83,7 @@ namespace TechnicalExercise.Models
         {
             using var connection = new MySqlConnection(_connectionString); // establish connection
 
+            // debugging
             Console.WriteLine($"book name: {book.BookName}");
             Console.WriteLine($"book id: {book.BookId}");
             Console.WriteLine($"customer name: {book.CustomerName}");
@@ -71,10 +94,10 @@ namespace TechnicalExercise.Models
             {
                 connection.Open(); // open connection
 
-                // update the book in the database to be reserved with customer name and customer email - we can use the same values to unreserve the book
+                // update the book in the database to be reserved with customer name and customer email - we can use the same values to unreserve the book if required
                 using var command = new MySqlCommand("UPDATE books_library SET reserved_booking = true, customer_name = @customerName, customer_email = @customerEmail, booking_number = @bookingNumber WHERE id = @bookId", connection);
                 
-                // updating the field values for the reserved book
+                // updating the field values for the reserved book safely
                 command.Parameters.AddWithValue("@bookId", book.BookId);
                 command.Parameters.AddWithValue("@customerName", book.CustomerName);
                 command.Parameters.AddWithValue("@customerEmail", book.CustomerEmail);
@@ -83,6 +106,32 @@ namespace TechnicalExercise.Models
                 command.ExecuteNonQuery(); // executes sql statement
             }
             catch(Exception e)
+            {
+                Console.WriteLine(e);
+                connection.Close(); // close connection
+            }
+        }
+
+        public void UnReserveBook(BookModel book)
+        {
+            using var connection = new MySqlConnection(_connectionString); // establish connection
+
+            try
+            {
+                connection.Open(); // open connection
+
+                // update the book in the database to unreserve the book
+                using var command = new MySqlCommand("UPDATE books_library SET reserved_booking = false WHERE id = @bookId, customer_name = @customerName, customer_email = @customerEmail, booking_number = @bookingNumber", connection);
+
+                // updating the field values for the reserved book safely
+                command.Parameters.AddWithValue("@bookId", book.BookId);
+                command.Parameters.AddWithValue("@customerName", null);
+                command.Parameters.AddWithValue("@customerEmail", null);
+                command.Parameters.AddWithValue("@bookingNumber", 0);
+
+                command.ExecuteNonQuery(); // executes sql statement
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e);
                 connection.Close(); // close connection
